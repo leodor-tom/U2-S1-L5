cbfd312666a71d7b9603346e6e35e2fc66ecbdef
leodor-tom <135353580+leodor-tom@users.noreply.github.com>
2023-07-28 17:46:04 +0200

    navbar

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("nav");
+const btn = document.querySelector(".menu-btn");
+document.addEventListener("scroll", (event) => {
+  if (window.scrollY > 400) {
+    nav.classList.add("white");
+    btn.classList.add("green");
+  } else {
+    nav.classList.remove("white");
+    btn.classList.remove("green");
+  }
+});
