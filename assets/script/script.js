d6e83df4d05809fca70f015cfc9990ac2db66125
leodor-tom <135353580+leodor-tom@users.noreply.github.com>
2023-07-28 18:11:41 +0200

    fix the  animation timig of the nav

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector("nav");
 const btn = document.querySelector(".menu-btn");
 document.addEventListener("scroll", (event) => {
-  if (window.scrollY > 400) {
+  if (window.scrollY >= 350) {
     nav.classList.add("white");
     btn.classList.add("green");
   } else {
